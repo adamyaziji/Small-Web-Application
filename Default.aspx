3c10ec0be0f41b151d4950a7c90e97ee6d587471
adamyaziji <ayaziji@hotmail.co.uk>
2015-08-25 14:22:08 +0100

    Added Files

---
+++ Default.aspx
@@ -0,0 +1,97 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 784px; width: 770px;">
+    <form id="form1" runat="server">
+    <div style="height: 38px; margin-left: 80px;">
+    
+        <asp:Label ID="Dob_msg" runat="server" Font-Bold="True" Font-Italic="True" Font-Underline="True" ForeColor="#0066FF" Text="First, please enter your date of birth"></asp:Label>
+        :<br />
+        <br />
+        <asp:Label ID="Day_lb" runat="server" Text="Day"></asp:Label>
+        &nbsp;(dd)
+        <asp:TextBox ID="Dateinput_tb" runat="server" Width="72px"></asp:TextBox>
+        &nbsp;
+        <asp:Label ID="Month_lb" runat="server" Text="Month"></asp:Label>
+        &nbsp;(mm)&nbsp;
+        <asp:TextBox ID="Monthinput_tb" runat="server" Width="75px"></asp:TextBox>
+        &nbsp;
+        <asp:Label ID="Year_lb" runat="server" Text="Year"></asp:Label>
+        &nbsp;(yyyy)&nbsp;
+        <asp:TextBox ID="Yearinput_tb" runat="server" Width="73px"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="dobEnter" runat="server" OnClick="dobEnter_Click" Text="Enter" />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Happybirthday_msg" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+
+        <asp:Label ID="Welcome_msg" runat="server" Font-Bold="True" Font-Italic="True" Font-Underline="True" ForeColor="#0066FF" Text="Ready to play Rock, Paper, Scissors, Lizard, Spock?"></asp:Label>
+        <br />
+        <br />
+        
+        <asp:RadioButton ID="Rockbutton" runat="server" Text="Rock" GroupName="choicelist" />
+        <br />
+        
+        <asp:RadioButton ID="Paperbutton" runat="server" Text="Paper" GroupName="choicelist" />
+        <br />
+       
+        <asp:RadioButton ID="Scissorsbutton" runat="server" Text="Scissors" GroupName="choicelist" />
+        <br />
+        
+        <asp:RadioButton ID="Lizardbutton" runat="server" Text="Lizard" GroupName="choicelist" />
+        <br />
+        
+        <asp:RadioButton ID="Spockbutton" runat="server" Text="Spock" GroupName="choicelist" />
+        <br />
+        <br />
+        <asp:Label ID="Rules_lb" runat="server" ForeColor="Red" Text="Please be aware of the following rules..."></asp:Label>
+        <br />
+        <br />
+        <ul>
+            <li> Scissors beats paper </li>
+            <li> Paper beats rock </li>
+            <li> Rock beats lizard </li>
+            <li> Lizard beats Spock </li>
+            <li> Spock beats scissors </li>
+            <li> Scissors beats lizard </li>
+            <li> Lizard beats paper  </li>
+            <li> Paper beats Spock  </li>
+            <li> Spock beats rock </li>
+            <li> Rock beats scissors </li.>
+            </li>
+        </ul>
+        
+        <br />
+        <br />
+
+        <asp:Button ID="playButton" runat="server" Height="29px" OnClick="playButton_Click" Text="Play!" Width="183px" />
+        
+        <asp:Button ID="playAgainButton" runat="server" Height="29px" OnClick="playAgainbutton_Click" Text="Play Again?" Width="175px" />
+        
+        <br />
+        <br />
+    
+        <asp:Label ID="computerChoice_lb" runat="server"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="result_lb" runat="server"></asp:Label>
+    
+        <br />
+        <br />
+
+    </div>
+        
+    <p>
+        &nbsp;</p>
+        
+    </form>
+    </body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string day, month, daymonth;
+    string today = DateTime.Now.ToString("dd-MM"); //convert todays time into a string
+    int comp;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void playButton_Click(object sender, EventArgs e)
+    {
+        Random random = new Random();
+        int comp = random.Next(0, 6); // randomise computers choice
+
+        /**
+         * These if statements determine which choice the computer
+         * has made and prints out its choice for the user to see
+         * once they have made a move.
+         **/
+
+
+        if (comp == 1)
+        {
+            computerChoice_lb.Text = "Computer has chosen Rock";
+        }
+
+        if (comp == 2)
+        {
+            computerChoice_lb.Text = "Computer has chosen Paper";
+        }
+
+        if (comp == 3)
+        {
+            computerChoice_lb.Text = "Computer has chosen Scissors";
+        }
+
+        if (comp == 4)
+        {
+            computerChoice_lb.Text = "Computer has chosen Lizard";
+        }
+
+        if (comp == 5)
+        {
+            computerChoice_lb.Text = "Computer has chosen Spock";
+        }
+
+
+        /**
+         * These sets of if statements determine whether the user or
+         * the computer has won, lost or if there is a draw
+         * depending on their choices. 
+         **/
+        if (Rockbutton.Checked == true)
+        {
+            if (comp == 3 || comp == 4)
+            {
+                result_lb.Text = "Congratulations, you win!";
+            }
+
+            else if (comp == 1)
+            {
+                result_lb.Text = "It's a draw!";
+            }
+            else
+            {
+                result_lb.Text = "Sorry, you lose!";
+
+            }
+        }
+
+        if (Paperbutton.Checked == true)
+        {
+            if (comp == 1 || comp == 5)
+            {
+                result_lb.Text = "Congratulations, you win!";
+            }
+            else if (comp == 2)
+            {
+                result_lb.Text = "It's a draw!";
+            }
+            else
+            {
+                result_lb.Text = "Sorry, you lose!";
+            }
+        }
+
+        if (Scissorsbutton.Checked == true)
+        {
+            if (comp == 2 || comp == 4)
+            {
+                result_lb.Text = "Congratulations, you win!";
+            }
+            else if (comp == 3)
+            {
+                result_lb.Text = "It's a draw!";
+            }
+            else
+            {
+                result_lb.Text = "Sorry, you lose!";
+            }
+        }
+
+        if (Lizardbutton.Checked == true)
+        {
+            if (comp == 2 || comp == 5)
+            {
+                result_lb.Text = "Congratulations, you win!";
+            }
+            else if (comp == 4)
+            {
+                result_lb.Text = "It's a draw!";
+            }
+            else
+            {
+                result_lb.Text = "Sorry, you lose!";
+            }
+        }
+
+        if (Spockbutton.Checked == true)
+        {
+            if (comp == 1 || comp == 3)
+            {
+                result_lb.Text = "Congratulations, you win!";
+            }
+            else if (comp == 5)
+            {
+                result_lb.Text = "It's a draw!";
+            }
+            else
+            {
+                result_lb.Text = "Sorry, you lose!";
+            }
+        }
+    }
+
+    /**
+     * This method deselects whichever radio button has been selected
+     * so the user can play again.
+     **/
+    protected void playAgainbutton_Click(object sender, EventArgs e)
+    {
+        Rockbutton.Checked = false;
+        Paperbutton.Checked = false;
+        Scissorsbutton.Checked = false;
+        Lizardbutton.Checked = false;
+        Spockbutton.Checked = false;
+    }
+
+    /**
+     * This method takes in whatever the user has typed into the
+     * day and month text boxes and creates a string based on (dd-mm).
+     * e.g. the string will contain "03-12".
+     * This string is then compared to the current date to see if it
+     * is their birthday.
+     **/
+    protected void dobEnter_Click(object sender, EventArgs e)
+    {
+        month = Monthinput_tb.Text;
+        day = Dateinput_tb.Text;
+        daymonth = (day + "-" + month);
+
+        if (daymonth == today)
+        {
+            Happybirthday_msg.Text = "Happy Birthday!!";
+        }
+    }
+}
